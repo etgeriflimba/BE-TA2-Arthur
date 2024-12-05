924ab20465f24d1ed8b4784a4c0eaada83985c33
Arthur <etgeriflimba@gmail.com>
2024-12-05 10:04:17 +0700

    CRUDuser

---
+++ backend/routes/UserRoute.js
@@ -0,0 +1,20 @@
+import express from "express";
+import { 
+    getUsers,
+    getUsersById,
+    createUser,
+    UpdateUser,
+    DeleteUser
+} from "../controllers/UserController.js";
+
+const router = express.Router();
+
+router.get('/users', getUsers);
+router.get('/users/:id', getUsersById);
+router.post('/users', createUser);
+router.patch('/users/:id', UpdateUser);
+router.delete('/users/:id', DeleteUser);
+
+
+
+export default router;
